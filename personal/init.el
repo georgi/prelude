4333a9656af27488259af1b2ac09ec130d0bcebd
Matthias Georgi <matti.georgi@gmail.com>
2013-12-10 22:30:37 +0100

    customizing

--- personal/init.el
+++ personal/init.el
@@ -1,39 +1,10 @@
-(require 'cl)
-(require 'package)
+(require 'helm-config)
+(require 'evil)
 
-(add-to-list 'package-archives
-   '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(require 'auto-complete-config)
+(require 'auto-complete-clang)
 
-(package-initialize)
-
-(defvar my-packages
-  '(auto-complete 
-    auto-complete-clang
-    ac-etags
-    ac-nrepl
-    ac-helm
-    dart-mode
-    ensime
-    evil
-    evil-indent-textobject
-    evil-paredit
-    evil-tabs
-    evil-matchit
-    surround
-    flycheck
-    )
-  "A list of packages to ensure are installed at launch.")
-
-(defun my-packages-installed-p ()
-  (loop for p in my-packages
-        when (not (package-installed-p p)) do (return nil)
-        finally (return t)))
-
-(unless (my-packages-installed-p)
-  (package-refresh-contents)
-  (dolist (p my-packages)
-    (when (not (package-installed-p p))
-      (package-install p))))
+;; (load-theme 'solarized-dark)
 
 (xterm-mouse-mode t)
 (defun track-mouse (e))
@@ -42,51 +13,29 @@
 (setq visible-bell 1)
 (setq ring-bell-function (lambda() ()))
 
-(fset 'yes-or-no-p 'y-or-n-p)
-(setq case-fold-search t)
-(setq enable-recursive-minibuffers nil)
-(setq inhibit-startup-screen t)
-(setq standard-indent 2)
-(setq tags-revert-without-query t)
-(setq-default tab-width 4)
+(setq prelude-whitespace nil)
 
-(setq initial-major-mode 'text-mode)
+(add-to-list 'ac-dictionary-directories (expand-file-name "~/.emacs.d/ac-dict"))
+(ac-config-default)
+(ac-set-trigger-key "TAB")
+(setq ac-quick-help-delay 0.5)
+(setq ac-auto-start nil)
 
-(tool-bar-mode 0)
-(column-number-mode t)
-(show-paren-mode t)
-(transient-mark-mode t)
-(recentf-mode)
-(xterm-mouse-mode t)
-(unless (window-system)
-  (menu-bar-mode 0))
-
-(require 'helm-config)
 (helm-mode)
-
-(require 'evil)
+(menu-bar-mode 0)
 (evil-mode 1)
-
-(load "~/.emacs.d/init.el")
+(global-auto-complete-mode t)
+(global-flycheck-mode)
 
 (global-set-key (kbd "<mouse-5>") 'scroll-up-line)
 (global-set-key (kbd "<mouse-4>") 'scroll-down-line)
 
 (global-set-key (kbd "DEL") 'backward-delete-char-untabify)
 
-(define-key evil-motion-state-map (kbd "<up>") 'evil-previous-line)
-(define-key evil-motion-state-map (kbd "<down>") 'evil-next-line)
-(define-key evil-motion-state-map (kbd "<left>") 'evil-backward-char)
-(define-key evil-motion-state-map (kbd "<right>") 'evil-forward-char)
-(define-key evil-motion-state-map (kbd "RET") 'insert-newline)
-
-(global-set-key (kbd "ESC <left>") 'evil-prev-buffer)
-(global-set-key (kbd "ESC <right>") 'evil-next-buffer)
-
 (global-set-key (kbd "C-c /") 'helm-do-grep)
 (global-set-key (kbd "C-c d") 'dired)
-;; (global-set-key (kbd "C-c i") 'helm-imenu)
-;; (global-set-key (kbd "C-c k") 'helm-show-kill-ring)
+(global-set-key (kbd "C-c i") 'helm-imenu)
+(global-set-key (kbd "C-c k") 'helm-show-kill-ring)
 ;; (global-set-key (kbd "C-c n") 'next-error)
 ;; (global-set-key (kbd "C-c o") 'helm-occur)
 ;; (global-set-key (kbd "C-c r") 'recompile)
