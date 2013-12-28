5e481fd91fbc27677af016a0d8407a04dfd0f5d2
Matthias Georgi <matthias@soundcloud.com>
2013-12-28 23:08:02 +0100

    remove safe hooks

--- personal/setup-go.el
+++ personal/setup-go.el
@@ -2,8 +2,8 @@
 
 (require 'go-mode-load)
 
-(add-hook 'before-save-hook 'gofmt-before-save)
-(add-hook 'before-save-hook 'go-remove-unused-imports)
+;; (add-hook 'before-save-hook 'gofmt-before-save)
+;; (add-hook 'before-save-hook 'go-remove-unused-imports)
 
 (add-hook 'go-mode-hook (lambda ()
                           (local-set-key (kbd "C-c i") 'go-goto-imports)))
